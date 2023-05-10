914026f2bc2acf1ea5804a7d6ba0ed7402014a99
mislamsahin <112614229+mislamsahin@users.noreply.github.com>
2023-05-10 18:38:26 +0300

    Dairenin Alan Ve Cevresini Hesaplama

---
+++ Dalanhesaplama.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Dalanhesaplama {
+    public static void main(String[] args) {
+        int r;
+        double pi = 3.14;
+        Scanner inpr = new Scanner(System.in);
+        System.out.print("Yarı Çap'ı Giriniz :");
+        r = inpr.nextInt();
+
+        double alan = pi * r * r ;
+        double cevre = 2 * pi * r ;
+
+        System.out.println("Alanı :" + alan);
+        System.out.print("Çevresi :" + cevre);
+    }
+}
